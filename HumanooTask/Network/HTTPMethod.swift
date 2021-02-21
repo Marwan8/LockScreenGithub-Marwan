fe3ce5cbd928604877c6f3190b579d3723717857
Marwan <marwan@Marwans-MacBook-Pro.local>
2021-02-21 12:56:24 +0100

    Implemented Requestable protocol for network handling

---
+++ HumanooTask/Network/HTTPMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPMethod.swift
+//  HumanooTask
+//
+//  Created by Marwan Ayman on 21.02.21.
+//
+
+enum HTTPMethod: String {
+    case get     = "GET"
+    case post    = "POST"
+    case put     = "PUT"
+    case delete  = "DELETE"
+}
